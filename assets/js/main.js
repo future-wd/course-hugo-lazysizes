6afc08dadd5579c20a16e93e12bd2cd0335e9a30
sean-pwd <sean@powerfulwebdesign.com.au>
2022-03-22 00:05:41 -0600

    fix: complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
 import 'lazysizes';
+import 'lazysizes/plugins/native-loading/ls.native-loading';
+// native loading config
+lazySizes.cfg.nativeLoading = {
+	setLoadingAttribute: true,
+	disableListeners: {
+		scroll: true
+	},
+};
 // import the whole bootstrap library
 // import * as bootstrap from 'bootstrap';
 
